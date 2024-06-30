36b61f4c62709a354d56b227da40c389980a240c
YipKei <53115298+YipKei@users.noreply.github.com>
2024-06-30 15:07:08 +0800

    修复龙战掉落鞘翅模板可能被传送门方块卡住的问题

--- src/main/java/com/yipkei/vanilladdition/mixin/EnderDragonFightMixin.java
+++ src/main/java/com/yipkei/vanilladdition/mixin/EnderDragonFightMixin.java
@@ -7,7 +7,6 @@ import net.minecraft.entity.boss.dragon.EnderDragonFight;
 import net.minecraft.item.ItemStack;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.Direction;
 import net.minecraft.world.Heightmap;
 import net.minecraft.world.gen.feature.EndPortalFeature;
 import org.jetbrains.annotations.Nullable;
