2ca7fadecdce4cc11325b305fec38fc42e8f7f7b
YipKei <53115298+YipKei@users.noreply.github.com>
2024-07-09 10:57:45 +0800

    增加玻璃的正确挖掘工具为镐，增加破坏王的适配方块

--- src/main/java/com/yipkei/vanilladdition/data/generator/ModBlockTagProvider.java
+++ src/main/java/com/yipkei/vanilladdition/data/generator/ModBlockTagProvider.java
@@ -37,6 +37,9 @@ public class ModBlockTagProvider extends FabricTagProvider.BlockTagProvider {
                 .add(ModBlocks.CLAY_WALL, ModBlocks.CLAY_STAIRS, ModBlocks.CLAY_SLAB);
 
         getOrCreateTagBuilder(BlockTags.PICKAXE_MINEABLE)
+                .add(Blocks.GLASS, Blocks.WHITE_STAINED_GLASS, Blocks.LIGHT_GRAY_STAINED_GLASS, Blocks.GRAY_STAINED_GLASS, Blocks.BLACK_STAINED_GLASS, Blocks.BROWN_STAINED_GLASS, Blocks.RED_STAINED_GLASS, Blocks.ORANGE_STAINED_GLASS, Blocks.YELLOW_STAINED_GLASS, Blocks.LIME_STAINED_GLASS, Blocks.GREEN_STAINED_GLASS, Blocks.CYAN_STAINED_GLASS, Blocks.LIGHT_BLUE_STAINED_GLASS, Blocks.BLUE_STAINED_GLASS, Blocks.PURPLE_STAINED_GLASS, Blocks.MAGENTA_STAINED_GLASS, Blocks.PINK_STAINED_GLASS)
+                .add(Blocks.GLASS_PANE , Blocks.WHITE_STAINED_GLASS_PANE, Blocks.LIGHT_GRAY_STAINED_GLASS_PANE, Blocks.GRAY_STAINED_GLASS_PANE, Blocks.BLACK_STAINED_GLASS_PANE, Blocks.BROWN_STAINED_GLASS_PANE, Blocks.RED_STAINED_GLASS_PANE, Blocks.ORANGE_STAINED_GLASS_PANE, Blocks.YELLOW_STAINED_GLASS_PANE, Blocks.LIME_STAINED_GLASS_PANE, Blocks.GREEN_STAINED_GLASS_PANE, Blocks.CYAN_STAINED_GLASS_PANE, Blocks.LIGHT_BLUE_STAINED_GLASS_PANE, Blocks.BLUE_STAINED_GLASS_PANE, Blocks.PURPLE_STAINED_GLASS_PANE, Blocks.MAGENTA_STAINED_GLASS_PANE, Blocks.PINK_STAINED_GLASS_PANE)
+
                 .add(ModBlocks.STEEL_BLOCK)
                 .add(ModBlocks.STONE_WALL)
                 .add(ModBlocks.SMOOTH_STONE_WALL, ModBlocks.SMOOTH_STONE_STAIRS)
@@ -135,6 +138,8 @@ public class ModBlockTagProvider extends FabricTagProvider.BlockTagProvider {
                 .add(Blocks.DEEPSLATE,Blocks.COBBLED_DEEPSLATE)
                 .add(Blocks.END_STONE, Blocks.END_STONE_BRICKS)
 
+                .add(Blocks.ANCIENT_DEBRIS ,Blocks.OBSIDIAN,Blocks.CRYING_OBSIDIAN)
+                .add(Blocks.VAULT, Blocks.TRIAL_SPAWNER, Blocks.REINFORCED_DEEPSLATE, Blocks.RESPAWN_ANCHOR)
 
         ;
 
