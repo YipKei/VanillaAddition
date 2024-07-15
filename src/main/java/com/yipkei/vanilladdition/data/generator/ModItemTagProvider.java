3162c7e0d26c077b183bee7bd68e30729944d2d4
YipKei <53115298+YipKei@users.noreply.github.com>
2024-07-15 16:19:27 +0800

    添加默认小电视旗和更多村民交易

--- src/main/java/com/yipkei/vanilladdition/data/generator/ModItemTagProvider.java
+++ src/main/java/com/yipkei/vanilladdition/data/generator/ModItemTagProvider.java
@@ -7,7 +7,6 @@ import net.fabricmc.fabric.api.datagen.v1.provider.FabricTagProvider;
 import net.minecraft.item.Items;
 import net.minecraft.registry.RegistryWrapper;
 import net.minecraft.registry.tag.ItemTags;
-import net.minecraft.registry.tag.TagKey;
 
 import java.util.concurrent.CompletableFuture;
 
