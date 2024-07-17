b3f12ec9e8f35a6db2511253956704222df21045
YipKei <53115298+YipKei@users.noreply.github.com>
2024-07-17 11:11:46 +0800

    黑暗深邃幻想转换地面龙头时保留朝向

--- src/main/java/com/yipkei/vanilladdition/item/DeepDarkFantasyWand.java
+++ src/main/java/com/yipkei/vanilladdition/item/DeepDarkFantasyWand.java
@@ -6,6 +6,7 @@ import net.minecraft.block.Blocks;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.sound.SoundCategory;
 import net.minecraft.sound.SoundEvents;
+import net.minecraft.state.property.Properties;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 
@@ -16,9 +17,9 @@ public class DeepDarkFantasyWand extends AbstractFairyWand{
 
     @Override protected void replaceOrDrop(World world, BlockPos blockPos, BlockState blockState, PlayerEntity player){
         if (blockState.isOf(Blocks.WITHER_SKELETON_SKULL)){
-
+            BlockState dragonHead = Blocks.DRAGON_HEAD.getDefaultState().with(Properties.ROTATION, blockState.get(Properties.ROTATION));
             world.playSound(null, blockPos, SoundEvents.ENTITY_DRAGON_FIREBALL_EXPLODE, SoundCategory.BLOCKS, 1.0f, 1.0f);
-            world.setBlockState(blockPos, Blocks.DRAGON_HEAD.getDefaultState(), Block.NOTIFY_ALL_AND_REDRAW);
+            world.setBlockState(blockPos, dragonHead, Block.NOTIFY_ALL_AND_REDRAW);
         }
         if (blockState.isOf(Blocks.WITHER_SKELETON_WALL_SKULL)){
             world.playSound(null, blockPos, SoundEvents.ENTITY_DRAGON_FIREBALL_EXPLODE, SoundCategory.BLOCKS, 1.0f, 1.0f);
